78022178afffc5a326d10807c7f7503523dcddca
iammarguuss <141366917+iammarguuss@users.noreply.github.com>
2024-03-10 09:30:15 +0100

    em)

--- 1.1/Program.cs
+++ 1.1/Program.cs
@@ -8,8 +8,10 @@ class Program
         double x = Convert.ToDouble(Console.ReadLine());
         Console.Write("Enter the value for a: ");
         double a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter the value for c: ");
+        double c = Convert.ToDouble(Console.ReadLine());
 
-        double y = Math.Pow(3 * (Math.Sqrt(x - a) + Math.Log(x) / (2 * Math.Sin(Math.PI / (3 + x)))), 1.0 / 3);
+        double y = Math.Cbrt(Math.Sqrt(c*x - a) + Math.Log(x) / (2 * Math.Sin(Math.PI / (3 + x))));
 
         Console.WriteLine("The value of the function is: " + y);
     }
