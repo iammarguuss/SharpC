f2d4a9b57a43bc11fa53a2b9109dc524563a347a
iammarguuss <141366917+iammarguuss@users.noreply.github.com>
2024-03-10 09:21:24 +0100

    Works?)

---
+++ 1.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the value for x: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter the value for a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        double y = Math.Pow(3 * (Math.Sqrt(x - a) + Math.Log(x) / (2 * Math.Sin(Math.PI / (3 + x)))), 1.0 / 3);
+
+        Console.WriteLine("The value of the function is: " + y);
+    }
+}
